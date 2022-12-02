d5f594182d226a70b77a30136e61d3b54839974d
Michael_SVAO <vierumihail7@gmail.com>
2022-12-02 17:08:25 +0300

    Задача №15

---
+++ dz2/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.WriteLine("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine()!);
+
+ if(a == 1){Console.WriteLine("Рабочий день");}
+ if(a == 2){Console.WriteLine("Рабочий день");}
+ if(a == 3){Console.WriteLine("Рабочий день");}
+ if(a == 4){Console.WriteLine("Рабочий день");}
+ if(a == 5){Console.WriteLine("Рабочий день");}
+ if(a == 6){Console.WriteLine("Выходной");}
+ if(a == 7){Console.WriteLine("Выходной");}
+ if(a > 7 || a < 1){Console.WriteLine("Несуществующее значение...");}
