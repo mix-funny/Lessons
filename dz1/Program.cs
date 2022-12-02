defbede0dfe8848a0a307509f1d7f283f788f0b0
Michael_SVAO <vierumihail7@gmail.com>
2022-12-02 15:09:50 +0300

    Задача №10

---
+++ dz1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите нужное число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int a1 = N/10;
+int a2 = a1%10;
+Console.WriteLine("Второе число: " + a2);
