9f3ab93de935a4116011f9fc1f9ff5bdef31ff0c
Michael_SVAO <vierumihail7@gmail.com>
2022-12-06 11:40:07 +0300

    Задача с 2д плоскостью

---
+++ lesson3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+
+Console.Clear();
+Console.Write("Введите координату xA ");
+int xA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату yA ");
+int yA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату xB ");
+int xB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату yB ");
+int yB = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt((xA-xB)*(xA-xB) + (yA-yB)*(yA-yB));
+Console.WriteLine(result);
