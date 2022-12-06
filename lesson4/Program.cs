5abaf9f0ef4161a31e5f679c043a5c77e2c7318a
Michael_SVAO <vierumihail7@gmail.com>
2022-12-06 12:01:28 +0300

    задача выводит квадрат числа

---
+++ lesson4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+//
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int num = 1;
+Console.WriteLine($"Таблица квадратов числа {N}, от 1 до {N}");
+while ( num < N )
+{
+   int result = num * num;
+   Console.Write(result + "," );
+   num ++;
+
+}
+Console.Write(N*N);
